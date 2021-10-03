b6a17ba4bda8b74d7a41076b9e369f275426036c
SofiFairyTell <58175476+SofiFairyTell@users.noreply.github.com>
2021-10-03 22:53:37 +0300

    add file

---
+++ Моделирование систем_/Отчеты/prj/lw_sm_1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace lw_sm_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.route = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.signal = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.signal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lw_sm_1.Properties.Resources.comp;
+            this.pictureBox1.Location = new System.Drawing.Point(531, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 106);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::lw_sm_1.Properties.Resources.comp;
+            this.pictureBox2.Location = new System.Drawing.Point(531, 170);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(198, 106);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::lw_sm_1.Properties.Resources.comp;
+            this.pictureBox3.Location = new System.Drawing.Point(531, 298);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(198, 106);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // route
+            // 
+            this.route.AutoSize = true;
+            this.route.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.route.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.route.Location = new System.Drawing.Point(185, 238);
+            this.route.Name = "route";
+            this.route.Size = new System.Drawing.Size(194, 81);
+            this.route.TabIndex = 3;
+            this.route.Text = "канал";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(249, 453);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(175, 54);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // signal
+            // 
+            this.signal.Location = new System.Drawing.Point(49, 258);
+            this.signal.Name = "signal";
+            this.signal.Size = new System.Drawing.Size(34, 38);
+            this.signal.TabIndex = 5;
+            this.signal.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 531);
+            this.Controls.Add(this.signal);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.route);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.signal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label route;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox signal;
+    }
+}
---
+++ Моделирование систем_/Отчеты/prj/lw_sm_1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lw_sm_1
+{
+    public partial class Form1 : Form
+    {       
+        Timer tmr = new Timer();
+       int sign = 1;
+       int min = 5, max = 10, labelValue = 5;
+        
+        public Form1()
+        {
+            InitializeComponent();  
+            tmr.Interval = 1000;          
+            tmr.Tick += new EventHandler(tmr_Tick);
+            route.Text = labelValue.ToString();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //e.Graphics.DrawRectangle(Pens.Blue, new Rectangle(10, 10, 100, 100));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Bitmap mybit = new Bitmap(signal.Width, signal.Height);
+            Graphics g = Graphics.FromImage(mybit);
+            SolidBrush grBrush = new SolidBrush(Color.Green);
+            g.FillRectangle(grBrush,0,0, signal.Width, signal.Height);
+            tmr.Enabled = !tmr.Enabled; //старт/стоп
+        }
+        void tmr_Tick(object sender, EventArgs e)
+        {
+            labelValue += sign;
+            route.Text = labelValue.ToString();
+            if (labelValue == max || labelValue == min)
+            {
+                sign *= -1;
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
