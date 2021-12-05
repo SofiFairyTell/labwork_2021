fb554a0ce33954bf68523528ef09478dfd7e3aa4
SKurbatova <sofiakurbatova@outlook.com>
2021-12-05 14:18:23 +0300

    add lab

---
+++ Моделирование систем_/Отчеты/prj/RandomStationaryNormalProcess/NormalProcessValueGenerator.cs
@@ -0,0 +1,39 @@
+﻿using RandomNumberGenerator;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomStationaryNormalProcess
+{
+    public class NormalProcessValueGenerator
+    {
+        private List<double> q;
+        private readonly double[] C;
+        private readonly double M;
+        private readonly Generator Generator = new Generator();
+
+        public NormalProcessValueGenerator(double[] C, double min, double max)
+        {
+            this.C = C;
+            M = (max + min) / 2;
+            q = new List<double>();
+            for (var i = 0; i < C.Length; i++)
+            {
+                q.Add(Generator.NormalDistributionFunction(1, 0));
+            }
+        }
+
+        private void ShiftQ()
+        {
+            q = q.Skip(1).ToList();
+            q.Add(Generator.NormalDistributionFunction(1, 0));
+        }
+
+        public double GetNextValue() 
+        {
+            var value = Enumerable.Range(0, C.Length).Select(i => C[i] * q[i]).Sum() + M;
+            ShiftQ();
+
+            return value;
+        }
+    }
+}
---
+++ Моделирование систем_/Отчеты/prj/RandomStationaryNormalProcessTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using RandomStationaryNormalProcess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomStationaryNormalProcessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new NormalProcessValueGenerator(
+                new double[] { 0.904, 0.191, 6.132, 2.447 },
+                40, 80);
+            var N = 1000;
+            var a = new List<double>();
+            var b = new List<double>();
+            for (var i = 0; i < N * 2; ++i)
+            {
+                if (i % 2 == 0)
+                {
+                    a.Add(generator.GetNextValue());
+                }
+                else
+                {
+                    b.Add(generator.GetNextValue());
+                }
+            }
+
+            var aM = a.Sum() / N;
+            var bM = b.Sum() / N;
+            var aD = 0d;
+            var bD = 0d;
+            for (var i = 0; i < N; i++)
+            {
+                aD += Math.Pow(a[i] - aM, 2) / (N - 1);
+                bD += Math.Pow(b[i] - bM, 2) / (N - 1);
+            }
+
+            var D = ((N - 1) * aD + (N - 1) * bD) / (N - 2);
+            var studentCriterion = Math.Sqrt((Math.Pow(aM - bM, 2) * N * N) / (D * N * 2));
+            Console.WriteLine($"Фрагмент чётных сгенерированных значений - [{string.Join(", ", a.Take(50))}]");
+            Console.WriteLine($"Критерий Стьюдента - {studentCriterion}");
+            _ = Console.ReadKey();
+        }
+    }
+}
