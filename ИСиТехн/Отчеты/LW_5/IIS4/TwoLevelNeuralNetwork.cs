747f05154245f071bfdef32391f2096b97223135
SKurbatova <sofiakurbatova@outlook.com>
2021-12-14 01:37:54 +0300

    update
    Простите....Когда-нибудь я займусь нейронными сетями по настоящему. Но пока это выглядит так. Потому что время на то, чтобы закрыть это все очень ограничено.

---
+++ ИСиТехн/Отчеты/LW_5/IIS4/TwoLevelNeuralNetwork.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IIS5
+{
+    public class TwoLevelNeuralNetwork
+    { 
+        private readonly Neuron[] Neurons;
+
+        private readonly Neuron FinalNeuron;
+
+        public TwoLevelNeuralNetwork(int entranceCount, int twoLevelCount = 2, double learningRate = 0.1)
+        {
+            Neurons = Enumerable.Range(0, twoLevelCount)
+                .Select(_ => new Neuron(entranceCount, learningRate))
+                .ToArray();
+            FinalNeuron = new Neuron(twoLevelCount, learningRate);
+        }
+
+        public double AskQuestion(double[] question)
+        {
+            var answers = Enumerable.Range(0, Neurons.Length)
+                .Select(i => Neurons[i].AskQuestion(question))
+                .ToArray();
+
+            return FinalNeuron.AskQuestion(answers);
+        }
+
+        public void Training(List<(double[] question, double answer)> tuples, int count)
+        {
+            for (var k = 0; k < count; k++)
+            {
+                foreach(var tuple in tuples)
+                {
+                    var answer = AskQuestion(tuple.question);
+                    if ((answer > 0.5 && tuple.answer != 1) || (answer <= 0.5 && tuple.answer == 1))
+                    {
+                        var question = Neurons.Select(x => x.Exit).ToArray();
+                        var entrancesError = FinalNeuron.RecountEntrancesWeightsByAnswer(question, tuple.answer);
+                        for (var i = 0; i < Neurons.Count(); i++)
+                        {
+                            Neurons[i].RecountEntrancesWeightsByError(tuple.question, entrancesError[i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
